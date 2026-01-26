c9d674b5128492b9316945edb9190fc4730c78f6
Mol-pedro <peddromn06@gmail.com>
2026-01-25 18:33:15 -0600

    a.ou

--- src/main.c
+++ src/main.c
@@ -1,7 +1,33 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(int argc, char *argv)
+int is_even(int number);
+
+int main()
 {
-    printf("Hello world!\n");
-    return 0;
+ printf("=== Par o impar ===\n");
+ int number;
+ printf("INSERTA UN NUMERO:");
+ scanf("%d", &number);
+
+ if(is_even(number) == 1)
+ {
+    printf("EL NUMERO ES PAR\n");
+ }
+ else
+ {
+    printf("EL NUMERO ES IMPAR\n");
+ }
+ return 0;
+}
+
+int is_even(int number)
+{
+    if(number %2 == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
