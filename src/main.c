4b223c0fef1d1621a2df5adfc2f35c83dd96565f
Mol-pedro <peddromn06@gmail.com>
2026-01-25 19:03:45 -0600

    Revert "a.ou"
    This reverts commit c9d674b5128492b9316945edb9190fc4730c78f6.

--- src/main.c
+++ src/main.c
@@ -1,33 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int is_even(int number);
-
-int main()
+int main(int argc, char *argv)
 {
- printf("=== Par o impar ===\n");
- int number;
- printf("INSERTA UN NUMERO:");
- scanf("%d", &number);
-
- if(is_even(number) == 1)
- {
-    printf("EL NUMERO ES PAR\n");
- }
- else
- {
-    printf("EL NUMERO ES IMPAR\n");
- }
- return 0;
-}
-
-int is_even(int number)
-{
-    if(number %2 == 0)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+    printf("Hello world!\n");
+    return 0;
 }
